bda149faa3df5433e4f5aaa8294d2390a7dc2e58
salman0H <salmanhashemi247@gmail.com>
2026-09-03 13:35:01 +0330

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,16 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
-import db from '@astrojs/db';
 import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
-  integrations: [react(), db()],
+  integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
     ssr: {
-      noExternal: ['gsap'], 
+      noExternal: ['gsap'],
     }
   },
 });
